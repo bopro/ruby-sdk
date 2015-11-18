73b94b4530d3b9fb8bf74a47f622df31358facaa
zxw <515856563@qq.com>
2015-11-18 21:47:54 +0800

    add fetch

--- lib/qiniu/management.rb
+++ lib/qiniu/management.rb
@@ -82,6 +82,12 @@ module Qiniu
           return HTTP.management_post(url)
         end # delete
 
+        def fetch(bucket, key, uri)
+          urlsafe_uri = Utils.urlsafe_base64_encode(uri)
+          url = Config.settings[:rs_host] + '/fetch' + urlsafe_uri + '/to/' + encode_entry_uri(bucket, key)
+          return HTTP.management_post(url)
+        end # fetch
+
         def batch(command, bucket, keys)
           execs = []
           keys.each do |key|
